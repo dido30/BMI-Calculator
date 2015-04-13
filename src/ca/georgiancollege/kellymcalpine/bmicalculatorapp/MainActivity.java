83f44e6673876067e27334052ee797b8f0f0c8c0
Kelly McAlpine <eiramyllek73@gmail.com>
2015-04-12 23:28:46 -0400

    Implemented methods for switch, buttons and BMI calculations
    Signed-off-by: Kelly McAlpine <eiramyllek73@gmail.com>

--- src/ca/georgiancollege/kellymcalpine/bmicalculatorapp/MainActivity.java
+++ src/ca/georgiancollege/kellymcalpine/bmicalculatorapp/MainActivity.java
@@ -2,8 +2,8 @@
  * PROGRAM NAME:  COMP1011-Assignment05
  * PROGRAM DESCRIPTION:  Android Application - BMI Calculator 
  * @author Kelly McAlpine 200260425
- * @version 0.3 - Initialization of all references 				
- * Date last modified:  2015-04-04
+ * @version 0.4 - Implemented methods for switch, buttons and BMI calculations				
+ * Date last modified:  2015-04-12
  */
 
 package ca.georgiancollege.kellymcalpine.bmicalculatorapp;
@@ -13,8 +13,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.DragEvent;
+import android.view.View;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -25,28 +28,37 @@ import android.util.AndroidException;
 
 public class MainActivity extends Activity 
 {
+	//INSTANCE VARIABLE (FIELDS)**************************************************************************
 	//Constants used when saving/restoring state
 	private static final String WEIGHT_INPUT = "WEIGHT_INPUT";
 	private static final String HEIGHT_INPUT = "HEIGHT_INPUT";
 	private static final String BMI_RESULT = "BMI_RESULT";
 	private static final String BMI_SCALE = "BMI_SCALE";
 	
-	private double userHeight; //height entered by user
-	private double userWeight; //weight entered by user
-	private double bmiCalculation; //BMI calculation based on user height & weight inputs
-	private TextView imperialTextView; //to display imperial measurements
-	private TextView metricTextView; //to display metric measurements
-	private EditText heightEditText; //displays user's height input
-	private EditText weightEditText; //displays user's weight input
-	private EditText resultEditText; //displays BMI calculation
-	private EditText bmiScaleEditText; //to display the BMI scale category (that calculation result falls into)
-	private Switch unitSwitch; //toggle between user's choice of imperial or metric
-	private Button calculateButton; //calculates the BMI based on the user's height and weight inputs
-	private Button resetButton;
+	private double _userHeight; //height entered by user
+	private double _userWeight; //weight entered by user
+	private double _heightInInches;
+	private double heightInMetres;
+	private double _weightInPounds;
+	private double _weightInKilograms;
+	private double _bmiImperialTotal;
+	private double _bmiMetricTotal;
+	private String _bmiScale;
+	private double _bmiCalculation; //BMI calculation based on user height & weight inputs
+	private TextView _imperialTextView; //to display imperial measurements
+	private TextView _metricTextView; //to display metric measurements
+	private EditText _heightEditText; //displays user's height input
+	private EditText _weightEditText; //displays user's weight input
+	private EditText _resultEditText; //displays BMI calculation
+	private EditText _bmiScaleEditText; //to display the BMI scale category (that calculation result falls into)
+	private Switch _unitSwitch; //toggle between user's choice of imperial or metric
+	private TextView _unitSwitchStatusTextView;
+	private Button _calculateButton; //calculates the BMI based on the user's height and weight inputs
+	private Button _resetButton;
+	private TextWatcher	_heightEditTextWatcher;
+	private TextWatcher	_weightEditTextWatcher;
 	
-	
-	
-
+    //PROTECTED OVERRIDE METHODS********************************************************************************
     @Override
     protected void onCreate(Bundle savedInstanceState) 
     {
@@ -56,75 +68,114 @@ public class MainActivity extends Activity
         //Check if app just started or is restoring from memory
         if (savedInstanceState == null)
         {
-        	this.userHeight = 0; // initialize the height amount as zero
-        	this.userWeight = 0; //initialize the weight amount as zero
-        	this.bmiCalculation = 0; //initialize the BMI calculation as zero
+        	this._userHeight = 0; // initialize the height amount as zero
+        	this._userWeight = 0; //initialize the weight amount as zero
+        	this._bmiCalculation = 0; //initialize the BMI calculation as zero
         } //end if
         else //app is being restored from memory, not executed from scratch
         {
         	//initialize the height, weight, BMI calculation & scale to saved amount
-        	this.userHeight = savedInstanceState.getDouble(HEIGHT_INPUT);
-        	this.userWeight = savedInstanceState.getDouble(WEIGHT_INPUT);
-        	this.bmiCalculation = savedInstanceState.getDouble(BMI_RESULT);
+        	this._userHeight = savedInstanceState.getDouble(HEIGHT_INPUT);
+        	this._userWeight = savedInstanceState.getDouble(WEIGHT_INPUT);
+        	this._bmiCalculation = savedInstanceState.getDouble(BMI_RESULT);
         	//this.bmiScaleEditText = savedInstanceState.getString(BMI_SCALE);
         } //end else
         
         //get references to the height, weight, BMI and scale editTexts
-        this.heightEditText = (EditText) findViewById(R.id.heightEditText);
-        this.weightEditText = (EditText) findViewById(R.id.weightEditText);
-        this.resultEditText = (EditText) findViewById(R.id.resultEditText);
-        this.bmiScaleEditText = (EditText) findViewById(R.id.BMIScaleEditText);
+        this._heightEditText = (EditText) findViewById(R.id.heightEditText);
+        this._weightEditText = (EditText) findViewById(R.id.weightEditText);
+        this._resultEditText = (EditText) findViewById(R.id.resultEditText);
+        this._bmiScaleEditText = (EditText) findViewById(R.id.BMIScaleEditText);
         
         //get the TextView displaying the Imperial & metric measurements
-        this.imperialTextView = (TextView) findViewById(R.id.imperialTextView);
-        this.metricTextView = (TextView) findViewById(R.id.imperialTextView);
+        this._imperialTextView = (TextView) findViewById(R.id.imperialTextView);
+        this._metricTextView = (TextView) findViewById(R.id.metricTextView);
         
-     // EditTextWatcher handles height & weight EditTexts' onTextChanged event
-        //this.heightEditText.addTextChangedListener(this.heightEditTextWatcher);
-        //this.weightEditText.addTextChangedListener(this.weightEditTextWatcher);
+        // EditTextWatcher handles height & weight EditTexts' onTextChanged event
+        this._heightEditText.addTextChangedListener(this._heightEditTextWatcher);
+        this._weightEditText.addTextChangedListener(this._weightEditTextWatcher);
         
+        //Used sample code for switch from http://www.mysamplecode.com/2013/04/android-switch-button-example.html
         //get the switch used to choose between measurement types
-        Switch unitSwitch = (Switch) findViewById(R.id.unitSwitch);
+        final Switch unitSwitch = (Switch) findViewById(R.id.unitSwitch);
+        this._unitSwitchStatusTextView = (TextView) findViewById(R.id.unitSwitchStatusTextView);
+        //set the switch to ON for Metric measurements 
+        //(OFF is the default state of the app which shows imperial measurements unless switched by the user)
+        this._unitSwitch.setChecked(true);
         
+        //attach a listener to check for changes in state
+        unitSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener()
+        {
+			private double	userHeight;
+			private Object	heightEditText;
+			private double	userWeight;
+			private Object	weightEditText;
+			private Object	resultEditText;
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+			{
+        		if (unitSwitch.isChecked())
+        		{
+        			_unitSwitchStatusTextView.setText("METRIC");
+        	    	//convert the user input into metric measurements
+        	    	heightInMetres = this.userHeight * 0.0254;
+        	    	((TextView) this.heightEditText).setText(String.format("%.02f", heightInMetres));
+        	    	_weightInKilograms = this.userWeight * 0.453592;
+        	    	((TextView) this.weightEditText).setText(String.format("%.02f", _weightInKilograms));
+        	    	
+
+        		}
+        		else
+        		{
+        			_unitSwitchStatusTextView.setText("IMPERIAL");
+        	    	//Set the default measurements to imperial
+        	    	_heightInInches = this.userHeight;
+        	    	((TextView) this.heightEditText).setText(String.format("%.02f", _heightInInches));
+        	    	_weightInPounds = this.userWeight;
+        	    	((TextView) this.weightEditText).setText(String.format("%.02f", _weightInPounds));
+        	    	
+        		}
+				
+			}
+			
+			//PUBLIC METHODS***********************************************************************************
+			public void onStartTrackingTouch(Switch unitSwitch){}
+			
+			public void onStopTrackingTouch(Switch unitSwitch) {}
+        });
+        
+               
         //get the buttons used to calculate the BMI and reset app
         Button calculateButton = (Button) findViewById(R.id.calculateButton);
         Button resetButton = (Button) findViewById(R.id.resetButton);
         
-        //this.unitSwitch.addOnLayoutChangeListener(this.unitSwitchListener);
-        //this.calculateButton.addTextChangedListener(this.calculateButton);
-    }
-    
-    //UTILITY METHODS**********************************************************
-    private void _bmiCalculations() //update & set measurements, cacluate BMI
-    {
-    	double heightInInches;
-    	double heightInMetres;
-    	double weightInPounds;
-    	double weightInKilograms;
-    	double bmiImperialTotal;
-    	double bmiMetricTotal;
-    	String bmiScale;
-    	
-    	//Set the default measurements to imperial
-    	heightInInches = this.userHeight;
-    	this.heightEditText.setText(String.format("%.02f", heightInInches));
-    	weightInPounds = this.userWeight;
-    	this.weightEditText.setText(String.format("%.02f", weightInPounds));
-    	
-    	//convert the user input into metric measurements
-    	heightInMetres = this.userHeight * 0.0254;
-    	this.heightEditText.setText(String.format("%.02f", heightInMetres));
-    	weightInKilograms = this.userWeight * 0.453592;
-    	this.weightEditText.setText(String.format("%.02f", weightInKilograms));
-    	
-    	//Imperial BMI Calculation
-    	bmiImperialTotal = (weightInPounds * 703) / (heightInInches * heightInInches);
-    	this.resultEditText.setText(String.format("%.02f", bmiImperialTotal));
-    	
-    	
-    	//Metric BMI Calculation
-    	bmiMetricTotal = weightInKilograms / (heightInMetres * heightInMetres);
-    	this.resultEditText.setText(String.format("%.02f", bmiMetricTotal));
+        //http://developer.android.com/guide/topics/ui/controls/button.html
+        //calculateButton response method
+        this._calculateButton.setOnClickListener(new View.OnClickListener()
+		{
+			
+			private TextView	resultEditText;
+
+			@Override
+			public void onClick(View v)
+			{
+				// TODO Auto-generated method stub
+				if(unitSwitch.isChecked())
+				{
+        	    	//Metric BMI Calculation
+        	    	_bmiMetricTotal = _weightInKilograms / (heightInMetres * heightInMetres);
+        	    	((TextView) this.resultEditText).setText(String.format("%.02f", _bmiMetricTotal));
+				}
+				else
+				{
+        	    	//Imperial BMI Calculation
+        	    	_bmiImperialTotal = (_weightInPounds * 703) / (_heightInInches * _heightInInches);
+        	    	((TextView) this.resultEditText).setText(String.format("%.02f", _bmiImperialTotal));
+				}
+			}
+		});
+
     }
 
     @Override
@@ -148,4 +199,5 @@ public class MainActivity extends Activity
         }
         return super.onOptionsItemSelected(item);
     }
-}
+    
+} //End of class MainActivity
